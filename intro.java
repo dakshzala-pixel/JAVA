03db60d5745e1a6ed4e7686b03b46eba26aa3b3f
DAKSH ZALA <dakshzala150@gmail.com>
2026-07-10 12:29:51 +0530

    Add files via upload

---
+++ intro.java
@@ -0,0 +1,14 @@
+// Write a java program for intro.
+
+public class intro
+{
+	public static void main(String args[])
+	{
+		
+		System.out.println("My name is Daksh Zala");
+		System.out.println("I am currently in second year of BCA.");
+		System.out.println("I have strong interest in programming.");
+		
+		
+	}
+}	
